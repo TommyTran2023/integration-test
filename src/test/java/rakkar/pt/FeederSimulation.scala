1562a8d17b4eb26d1ac98aad2b10706134d6a82c
TommyTran2023 <tommy.tran@rakkardigital.com>
2023-10-11 09:54:08 +0700

    comment for scalaSimulator

--- src/test/java/rakkar/pt/FeederSimulation.scala
+++ src/test/java/rakkar/pt/FeederSimulation.scala
@@ -1,27 +1,27 @@
-package rakkar.pt
+// package rakkar.pt
 
-import com.intuit.karate.gatling.PreDef._
-import io.gatling.core.Predef._
+// import com.intuit.karate.gatling.PreDef._
+// import io.gatling.core.Predef._
 
-import scala.concurrent.duration._
-import scala.util.Random
-import scala.io.Source
+// import scala.concurrent.duration._
+// import scala.util.Random
+// import scala.io.Source
 
-class FeederSimulation extends Simulation {
+// class FeederSimulation extends Simulation {
 
-  val protocol = karateProtocol()
+//   val protocol = karateProtocol()
 
-  protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")
+//   protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")
 
-  val requester = csv("src/test/java/data/login.csv").circular
+//   val requester = csv("src/test/java/data/login.csv").circular
 
-  val scn = scenario("FeedSimulation").exec {
-              feed(requester)
-              .exec(karateSet("requesterUserName", session => session("userName").as[String]))
-              .exec(karateFeature("classpath:rakkar/pt/PT_FilterTransactions.feature"))
-            }
+//   val scn = scenario("FeedSimulation").exec {
+//               feed(requester)
+//               .exec(karateSet("requesterUserName", session => session("userName").as[String]))
+//               .exec(karateFeature("classpath:rakkar/pt/PT_FilterTransactions.feature"))
+//             }
             
-  setUp(
-    scn.inject(rampUsers(1) during (1 seconds))
-  ).protocols(protocol)
-}
+//   setUp(
+//     scn.inject(rampUsers(1) during (1 seconds))
+//   ).protocols(protocol)
+// }

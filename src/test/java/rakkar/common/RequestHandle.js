9d6b9964e5bb9888327b72b1442ade3dbbfdb164
TommyTran2023 <tommy.tran@rakkardigital.com>
2024-01-12 18:59:24 +0700

    update svc

--- src/test/java/rakkar/common/RequestHandle.js
+++ src/test/java/rakkar/common/RequestHandle.js
@@ -21,7 +21,7 @@ function fn(){
         if (requestId != null){
             karate.log('Cancel requestId: ' + requestId)
             var biometric = karate.call(svc + 'Biometric.feature@RequesterDoBiometric')
-            var cancel = karate.call(svc + 'AdvanceQuorum.feature@CancelRequest', {requestId:requestId , challengeAnswerRequest: biometric.challengeAnswerRequest} ) 
+            var cancel = karate.call(svc + 'Quorums.feature@CancelRequest', {requestId:requestId , challengeAnswerRequest: biometric.challengeAnswerRequest} ) 
 
             if (cancel.responseStatus != 200)
                 throw new TypeError('Cannot cancel request. Error: ' + JSON.stringify(cancel, null, 4))

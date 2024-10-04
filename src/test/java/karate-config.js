a0843778c80776233eee86c20da81f7ea3cd2862
Tommy Tran <tommy.tran@rakkardigital.com>
2024-10-04 14:00:53 +0700

    test jenkins credentials

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -35,10 +35,10 @@ function fn () {
     
     // Load secret
     if (karate.properties['runMode'] == 'JENKINS'){
-        var path = karate.env('SECRET');
-        var secret = karate.read(path);
-        karate.set('privateKey', secret.secret);
-        console.log('-->>>>:' + secret.secret);
+        // var path = karate.env('SECRET');
+        // var secret = karate.read(path);
+        // karate.set('privateKey', secret.secret);
+        // console.log('-->>>>:' + secret.secret);
     }
     else if (fileUtils.isFileExist(secret)){
         try {

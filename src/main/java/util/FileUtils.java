b6949ec1463d3f5cd7babdf9b65c52833c1fba8d
Tommy Tran <tommy.tran@rakkardigital.com>
2024-10-07 21:31:53 +0700

    revert code

--- src/main/java/util/FileUtils.java
+++ src/main/java/util/FileUtils.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 
 public class FileUtils {
     public static Map<String, Object> DataListMap = new HashMap<>();

246e8d6efe2aebedf35af45cf9beb0b205efb34b
Tommy Tran <tommy.tran@rakkardigital.com>
2024-10-21 17:26:50 +0700

    remove pupeteer executablePath

--- figment_wc.js
+++ figment_wc.js
@@ -30,8 +30,7 @@ async function getQRCode() {
 async function getQR() {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
   const page = await browser.newPage();
 

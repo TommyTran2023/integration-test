ed92be27cd6f9a8b05c2e7bc7faeba3b66d9a869
Tommy Tran <tommy.tran@rakkardigital.com>
2024-10-21 17:16:33 +0700

    add executablePath

--- figment_wc.js
+++ figment_wc.js
@@ -31,6 +31,7 @@ async function getQR() {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
   });
   const page = await browser.newPage();
 
